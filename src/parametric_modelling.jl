d3f8c510376034fbe8d3b8d76eb40826165080c9
James Lyons <james.lyons0@gmail.com>
2014-02-11 16:37:18 +1000

    Update parametric_modelling.jl

--- src/parametric_modelling.jl
+++ src/parametric_modelling.jl
@@ -1,9 +1,29 @@
 module parametric_modelling
 
-import linear_predictive_coding: levinson
+export levinson, aryule, arcov, armcov
 
-export aryule, arcov, armcov
+# levinson: levinson durbin recursion
+# takes vector of autocorrelation coefficients 'R' and
+# returns a 3-tuple of linear prediction coefficients, Gp^2 of the filter, 
+# and reflection coefficents
+function levinson(R::Vector,L::Integer)
+    a = zeros(L,L)
+    P = zeros(1,L)
  
+    # for m = 1
+    a[1,1] = -R[2]/R[1]
+    P[1] = R[1]*(1-a[1,1]^2)
+ 
+    # for m = 2,3,4,..L
+    for m = 2:L
+        a[m,m] = (-(R[m+1] + dot(vec(a[m-1,1:m-1]),R[m:-1:2]))/P[m-1])
+        a[m,1:m-1] = a[m-1,1:m-1]+a[m,m]*a[m-1,m-1:-1:1]
+        P[m] = P[m-1]*(1-a[m,m]^2)
+    end
+    [1., vec(a[L,:])], P[L], diag(a)
+end
+
+
 function aryule(x::Vector,L::Integer)
     if length(x) < L
         error("aryule: model order ($L) can't be more than length(x) ($(length(x))).")
